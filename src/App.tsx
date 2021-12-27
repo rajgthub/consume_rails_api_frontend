1066bc65be48bbf4cd2805bb328780b0d94a986b
raj <rraju12@gmail.com>
2021-12-27 22:18:10 +0000

    Base React app to consume rails api.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [contacts, setContacts] = useState([])
+  useEffect(() => {
+    
+    (async function(){
+      if(!document.cookie?.split(";")[0].split("=")[1]){
+        const {data} = await axios.post("http://localhost:3000/v1/sessions?email=rraju@gmail.com&password=123456")
+        document.cookie = `appAuth=${data.data.jwt_token}`
+      }
+
+      const contact = await axios.post("http://localhost:3000/v1/contacts", 
+      {
+        "first_name": "contact two first:REacg name ...",
+        "last_name": "contact two last:React name",
+        "email": "rraju12@gmail.com",
+       },
+       {
+         headers: {
+           Authorization: 'Bearer ' + document.cookie.split(";")[0].split("=")[1]
+         }
+       })
+       const contacts = await axios.get("http://localhost:3000/v1/contacts", { withCredentials: true })
+       setContacts(contacts.data)
+    })();
+  
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{JSON.stringify(contacts)}</p>
     </div>
   );
 }
